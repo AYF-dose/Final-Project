669142a29930724bdf1b3d10c3c7bcbdaa2cd276
stefiana24 <129258907+stefiana24@users.noreply.github.com>
2023-06-15 18:47:08 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import AboutPage from "/src/components/AboutPage";
+import GridBerita from "/src/components/GridBerita";
+import Banner from "/src/components/Banner";
+import Team from "/src/components/Team";
+
+const App = () => {
+  return (
+    <div>
+      <AboutPage />
+      <GridBerita />
+      <Banner />
+      <Team />
+    </div>
+  );
+};
+
+export default App;
